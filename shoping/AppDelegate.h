1d6a9ec97676ff199d17dd1815aed2f2a257a742
深圳鲲鹏 <apple@shinsenkunpengmatoiMac-21.local>
2013-12-11 15:00:40 +0800

    first

---
+++ shoping/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shoping
+//
+//  Created by 深圳鲲鹏 on 13-12-11.
+//  Copyright (c) 2013年 i10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
